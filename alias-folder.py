90ab24b60e92c70a494f5cde6256ff65229d3f5f
Ted Melot <tmelot@getfused.com>
2018-03-10 00:10:35 -0600

    Implement full cycle of functionality
    Menu item only active on top-level folders, enter an alias to change it and write to the project file, entering nothing or the default display name will clear an alias, little refactoring, docs

--- alias-folder.py
+++ alias-folder.py
@@ -2,6 +2,15 @@ import sublime_plugin
 import functools
 
 class AliasFolderCommand(sublime_plugin.WindowCommand):
+
+	def is_enabled(self, paths=[]):
+		'''
+			Returns true if there is exactly one requested path and it matches one of the top-level folders in the
+			current project.
+		'''
+		return len(paths) == 1 and self.get_project_folder_index(paths[0]) >=0
+
+
 	def run(self, *args, **kwargs):
 		'''
 			Called by Sublime when this command is run. Validates that you've only selected one folder and then opens
@@ -11,28 +20,51 @@ class AliasFolderCommand(sublime_plugin.WindowCommand):
 		# TODO: Error if more than one selected
 		selectedPath = kwargs['paths'][0]
 
+		# Get current alias or name
+		projectData = self.window.project_data()
+		item = projectData['folders'][self.get_project_folder_index(selectedPath)]
+		if 'name' in item:
+			currentName = item['name']
+		else:
+			currentName = item['path'].split('/')[-1]
+
 		# aah lots of barking! afk ... oh, just packages, bou was IN THE AIR haha
 		panel = self.window.show_input_panel(
-			"New folder alias:",
-			"currentMenuItem",
-			functools.partial(self.on_done, selectedPath=str(selectedPath)),
+			"Alias this folder (enter nothing to clear an alias):",
+			currentName,
+			functools.partial(self.on_done, path=str(selectedPath)),
 			None,
 			None
 		)
 
-	def on_done(self, alias, selectedPath):
+
+	def on_done(self, alias, path):
+		'''
+			TODO
 		'''
 
-		'''
-
-		# print("alias = {}, selectedPath = {}".format(alias, selectedPath))
+		index = self.get_project_folder_index(path)
 		projectData = self.window.project_data()
 
-		# Genexp to get index of the folder from the current project file. next() to get the first value from the
-		# genexp object.
-		foundIndex = next((index for index, folder in enumerate(projectData['folders']) if folder['path'] == selectedPath))
-
-		if foundIndex >= 0:
-			print("found at index {}".format(foundIndex))
+		# Clear alias if empty or the same as the last part of the path
+		if alias == '' or alias == path.split('/')[-1]:
+			projectData['folders'][index].pop('name')
 		else:
-			print("not found!!!!!")
+			projectData['folders'][index]['name'] = alias
+
+		self.window.set_project_data(projectData)
+
+
+	def get_project_folder_index(self, path):
+		'''
+			Searches the current project's folders list for an item with the given path, returns the index if found,
+			otherwise -1.
+		'''
+
+		folders = self.window.project_data().get('folders')
+		# Listcomp to get index of the folder with the given path.
+		foundIndex = [index for index, folder in enumerate(folders) if folder['path'] == path]
+		if len(foundIndex) > 0:
+			return foundIndex[0]
+		else:
+			return -1
